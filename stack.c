c45f245a2adb1c3fb430540d9931e6a71bb0faad
cwk@macbook <cehn2000@163.com>
2026-07-10 18:01:01 +0800

    卡在了栈的二级指针指向最终成员的语法上

--- stack.c
+++ stack.c
@@ -1,23 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // 栈的基本运算：初始化、判空、进栈、退栈、取栈顶
 
 // 顺序栈定义
+#define MAXSIZE 10
+typedef struct
+{
+    int data[MAXSIZE];
+    int top;
+} SqStack;
 
 // 初始化
+void init_SqStack(SqStack *s)
+{
+    s->top = -1;
+}
 
 // 判空
+bool is_empty(SqStack *s)
+{
+    return s->top == -1;
+}
 
 // 进栈
+bool push(SqStack *s, int data)
+{
+    if(s->top >= MAXSIZE -1) return false;
+    s->data[++s->top] = data;
+    return true;
+}
 
 // 退栈
+bool pop(SqStack *s, int *data)
+{
+    if(is_empty(s)) return false;
+    *data = s->data[s->top];
+    s->top--;
+    return true;
+}
 
 // 取栈顶
+bool get_top(SqStack *s, int *data)
+{
+    if(is_empty(s)) return false;
+    *data = s->data[s->top];
+    return true;
+}
 
 // 链栈定义
+typedef struct
+{
+    int data;
+    LNode *next;
+} LNode, *LinkStack;
 
 // 初始化
+void init_LinkStack(LinkStack *s)
+{
+    *s = (LinkStack)malloc(sizeof(LNode));
+    
+}
 
 // 判空
 
